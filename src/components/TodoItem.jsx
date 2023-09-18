ea647f63319467c8beb807160410274bf904964a
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-09-18 14:16:27 +0530

    Add confirm message when deleting todo

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -49,7 +49,11 @@ function TodoItem({ todo }) {
       {/* Delete Todo Button */}
       <button
         className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
-        onClick={() => deleteTodo(todo.id)}
+        onClick={() => {
+          if (confirm("Are you sure you want to delete this todo?")) {
+            deleteTodo(todo.id)
+          }
+        }}
       >
         ❌
       </button>
