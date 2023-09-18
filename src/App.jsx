c37973dbeb69934b63a7cb1715a303f813747651
Kuldeep Chotaliya <kuldeep.chotaliya@bacancy.com>
2023-09-18 13:16:06 +0530

    Fix indentation

--- src/App.jsx
+++ src/App.jsx
@@ -41,21 +41,21 @@ function App() {
     <TododContextProvider value={{ todos, setTodos, updateTodo, deleteTodo, toggleComplete }}>
       <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
         <div className="bg-[#172842] min-h-screen py-8">
-            <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
-            <div className="mb-4">
-              <TodoForm />
-            </div>
-            <div className="flex flex-wrap gap-y-3">
-              {
-                todos.map((todo) => {
-                  return(
-                    <div key={todo.id}>
-                      <TodoItem todo={todo} />
-                    </div>
-                  )
-                })
-              }
-            </div>
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {
+              todos.map((todo) => {
+                return(
+                  <div key={todo.id}>
+                    <TodoItem todo={todo} />
+                  </div>
+                )
+              })
+            }
+          </div>
         </div>
       </div>
     </TododContextProvider>
